80338f51ec16376e1327d8f5cd5d86c88c2cb212
Ionică Bizău <bizauionica@yahoo.com>
2014-11-23 12:25:40 +0200

    Tests

--- test/index.js
+++ test/index.js
@@ -1,8 +1,7 @@
 var AnsiParser = require("../lib/index.js")
   , Couleurs = require("couleurs")()
-  , input = Couleurs.rgb("Hello World!", 255, 0, 0)
+  , input = Couleurs.rgb("Hello", 255, 0, 0) + " World! " + Couleurs.rgb("Isn't this cool?", 0, 255, 0) + " ";
   ;
 
 console.log(input);
-console.log(AnsiParser.parse(input));
-//console.log(AnsiParser.stringify(AnsiParser.parse(input)));
+console.log(AnsiParser.stringify(AnsiParser.parse(input)));
