2d4208fa4bee3ea09aeadaab46846fd9ed226a6d
Ionică Bizău <bizauionica@yahoo.com>
2014-12-06 19:27:52 +0200

    Some tests

--- test/index.js
+++ test/index.js
@@ -1,9 +1,21 @@
 var AnsiParser = require("../lib/index.js")
+  , Fs = require("fs")
   , Couleurs = require("couleurs")()
-  , input = Couleurs.rgb("Hello", 255, 0, 0) + " World! " + Couleurs.rgb("Isn't this cool?", 0, 255, 0);
+  , tests = [
+        Couleurs.rgb("Text with one color", 255, 0, 0)
+      , Couleurs.bold("Bold text", 255, 0, 0)
+      , Couleurs.rgb("Hello", 255, 0, 0) + " World! " + Couleurs.rgb("Isn't this cool?", 0, 255, 0)
+      , Fs.readFileSync("./vim.txt", "utf-8")
+    ];
   ;
 
-console.log(input);
-console.log(AnsiParser.stringify(AnsiParser.parse(input)));
 
-console.log(AnsiParser.stringify(AnsiParser.parse("Non colored.")));
+for (var i=0; i < tests.length; ++i) {
+    console.log(">>> Test " + (i + 1));
+    console.log("> Input:");
+    console.log(tests[i]);
+    console.log("> Parsed and stringified:");
+    var out = tests[i];
+    console.log("Computed. Showing.");
+    console.log(AnsiParser.stringify(AnsiParser.parse(out)));
+}
